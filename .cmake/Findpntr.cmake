a4cbbd834d5494dc9173eb09c6aa14886e9c9d56
David Konsumer <83857+konsumer@users.noreply.github.com>
2023-10-22 14:08:44 -0700

    Initial commit

---
+++ .cmake/Findpntr.cmake
@@ -0,0 +1,8 @@
+# pntr
+include(FetchContent)
+FetchContent_Declare(
+    pntr
+    GIT_REPOSITORY https://github.com/robloach/pntr.git
+    GIT_TAG b72b45139f5b9145dab215d2cbb4008af56f8901
+)
+FetchContent_MakeAvailable(pntr)
---
+++ .cmake/Findpntr_app.cmake
@@ -0,0 +1,8 @@
+# pntr_app
+include(FetchContent)
+FetchContent_Declare(
+    pntr_app
+    GIT_REPOSITORY https://github.com/robloach/pntr_app.git
+    GIT_TAG 84cc3ca3d3eed123e5fd84063ef40fda30405b05
+)
+FetchContent_MakeAvailable(pntr_app)
